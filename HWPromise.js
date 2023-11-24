601d9dd0342c67061013f9eb368549bc6ed12630
daria kutluieva <daryakutlueva@gmail.com>
2023-11-24 20:32:06 +0100

    first

---
+++ HWPromise.js
@@ -0,0 +1,198 @@
+// 1.1 Что выведет следующий код
+console.log("start");
+
+const promise11 = new Promise((resolve, reject) => {
+  console.log(1);
+});
+
+const fn1 = () => {
+  console.log("3");
+};
+
+console.log("end");
+
+// start 1 end
+
+// 1.2 Какой результат выполнения этого кода?
+
+console.log("start");
+
+const promise12 = new Promise((resolve, reject) => {
+  console.log(1);
+  resolve(200);
+});
+
+promise12.then((response) => {
+  console.log(response);
+});
+
+console.log("end");
+// start 1 end 200
+
+
+// 1.3 Какой результат выполнения этого кода?
+
+console.log("start");
+
+const promise13 = new Promise((resolve, reject) => {
+  console.log(1);
+});
+
+promise13.then((res) => {
+  console.log(2);
+});
+
+console.log("end");
+// start 1 end
+
+
+// 1.4 Какой результат выполнения этого кода?
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("setTimeout");
+});
+
+Promise.resolve().then(() => {
+  console.log("resolve");
+});
+
+console.log("end");
+// start end resolve setTimeout
+
+
+
+// 1.5
+
+function job() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("hello world");
+    }, 2000);
+  });
+}
+
+job().then((data) => console.log(data));
+// "hello world
+
+
+// 1.6
+
+function job(data) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(data)) {
+      reject("error");
+    } else if (data % 2 === 0) {
+      setTimeout(() => {
+        reject("even");
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        resolve("odd");
+      }, 1000);
+    }
+  });
+}
+
+job("aasd")
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+job(2)
+  .then((data) => console.log(data))
+  .catch((error) => console.log("rejected: " + error));
+
+job(3).then((data) => console.log(data));
+
+//"error", "rejected: even" , "odd"
+
+
+
+
+// 1.7
+console.log("start");
+
+const promise1 = new Promise((resolve, reject) => {
+  console.log(1);
+
+  resolve(200);
+  console.log(3);
+});
+
+promise1.then((res) => {
+  console.log(res);
+});
+
+console.log("end");
+
+
+//start 1 3 end 200
+
+//1.8
+console.log("start");
+
+const fn = () =>
+  new Promise((resolve, reject) => {
+    console.log(1);
+    resolve("success");
+  });
+
+console.log("middle");
+
+fn().then((res) => {
+  console.log(res);
+});
+
+console.log("end");
+
+//start middle 1 end success
+
+
+//1.9
+const promise = new Promise((resolve, reject) => {
+  console.log(1);
+
+  setTimeout(() => {
+    console.log("timerStart");
+    resolve("success");
+    console.log("timerEnd");
+  }, 0);
+
+  console.log(2);
+});
+
+promise.then((res) => {
+  console.log(res);
+});
+
+console.log(4);
+
+// 1 2 4 timerStart timerEnd success
+
+//1.10
+console.log(1);
+
+setTimeout(() => console.log(2));
+
+Promise.resolve().then(() => console.log(3));
+
+Promise.resolve().then(() => setTimeout(() => console.log(4)));
+
+Promise.resolve().then(() => console.log(5));
+
+setTimeout(() => console.log(6));
+
+console.log(7);
+//1 7 3 5 2 6 4
+
+
+// 1.11
+let promise1111 = new Promise(function (resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise1111.then((data) => console.log(data));
+// 1
